f62b18183f041c2a6e729a72bad52c621b0f91eb
Jordan Thimmig <yogibuff@icloud.com>
2015-11-20 13:17:16 -0800

    Update fizzbuzz.js
    added alternate, slightly more concise version

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,30 @@
 for (var i=1 ; i<=20 ; i++){
   if (i%3===0 && i%5!=0){
     console.log("Fizz");
-  } else if (i%5===0 && i%3!=0){
+  } 
+  else if (i%5===0 && i%3!=0){
     console.log("Buzz");
-  } else if (i % 3 === 0 && i % 5 === 0 ){
+  } 
+  else if (i%3 ===0 && i%5 ===0){
     console.log("FizzBuzz");    
-  } else {
+  } 
+  else {
+    console.log (i);
+  }
+}
+
+// alternate, slightly more concise version //
+for(var i=1; i<=20; i++){
+  if (i%3===0 && i%5===0){
+    console.log("FizzBuzz");
+  }
+  else if (i%3===0){
+    console.log("Fizz");
+  }
+  else if (i%5===0){
+    console.log("Buzz");
+  }
+  else {
     console.log (i);
   }
 }
