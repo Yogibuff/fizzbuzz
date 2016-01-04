51c92cfb937cb18b3695987a998fe3181021932f
Jordan Thimmig <yogibuff@icloud.com>
2016-01-04 13:01:23 -0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,11 @@
-// javascript ex. 1 //
+/* 
+Create a loop that prints the numbers 1 through 20 to the console.
+For each number that is evenly divisible by 3 print the word fizz, instead of the number.
+For each number that is evenly divisible by 5 print the word buzz, instead of the number.
+For each number that is evenly divisible by both 3 and 5 print the word fizzbuzz, instead of the number. 
+*/
+
+// Javascript FizzBuzz Solution //
 
 for (var i=1 ; i<=20 ; i++){
   if (i%3===0 && i%5!=0){
